4367f9fe5c027cdf473162ffe2a26bab5ebd63bb
Keegan <keeganfinegan@gmail.com>
2021-06-03 17:06:09 +1000

    added tests for AssetService and OrganisationalUnitService classes

--- src/main/java/com/supplyr/supplyr/startup/AspectJConfig.java
+++
@@ -1,9 +0,0 @@
-package com.supplyr.supplyr.startup;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
-
-@ComponentScan
-@EnableSpringConfigured
-public class AspectJConfig {
-}
